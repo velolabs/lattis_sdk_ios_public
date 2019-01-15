e654c2faadd282f0f5551ef1ea2252a9e9b1ae50
Ravil Khusainov <zver.kaban@gmail.com>
2019-01-15 12:51:29 +0300

    Pods updated

--- Lattis Demo/Pods/Target Support Files/Pods-Lattis Demo/Pods-Lattis Demo-frameworks.sh
+++ Lattis Demo/Pods/Target Support Files/Pods-Lattis Demo/Pods-Lattis Demo-frameworks.sh
@@ -145,10 +145,12 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/CryptoSwift/CryptoSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GradientCircularProgress/GradientCircularProgress.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/KeychainSwift/KeychainSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/CryptoSwift/CryptoSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GradientCircularProgress/GradientCircularProgress.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/KeychainSwift/KeychainSwift.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
