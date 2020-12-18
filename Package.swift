705536c6858b2928864d43c044ee09af96b56d26
Ravil Khusainov <zver.kaban@gmail.com>
2020-12-18 07:07:35 +0300

    Fix package typo

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "LattisSDK",
-            type: .static,
+//            type: .static,
             targets: ["LattisSDK"]),
     ],
     dependencies: [
@@ -26,9 +26,10 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+//        .target(name: "LattisSDK", dependencies: ["LattisSDK"]),
         .binaryTarget(
             name: "LattisSDK",
-            path: "Lattis.xcworkspace")
+            path: "LattisSDK.xcframework")
 //            dependencies: [
 //                .product(name: "KeychainSwift", package: "KeychainSwift")
 //            ])
