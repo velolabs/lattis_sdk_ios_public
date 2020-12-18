38095d84afc8d6340a403008ed4d8502619bcdef
Ravil Khusainov <zver.kaban@gmail.com>
2020-12-18 06:58:29 +0300

    Library type changed to static

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "LattisSDK",
-            type: .dynamic,
+            type: .static,
             targets: ["LattisSDK"]),
     ],
     dependencies: [
